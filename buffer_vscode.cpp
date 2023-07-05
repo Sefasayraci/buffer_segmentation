d9bed813a6f631a245f4891639a0676717cabb6b
Sefa Sayracı <73780930+Sefasayraci@users.noreply.github.com>
2023-07-05 16:48:31 +0300

    Update buffer_vscode.cpp

--- buffer_vscode.cpp
+++ buffer_vscode.cpp
@@ -1,3 +1,4 @@
+// C++ Kodu
 /*****************************This is a code for changing the letters that repeat on the code*****************************************/
 #include <iostream>
 #include <cstring>
@@ -37,3 +38,41 @@ int main()
     return 0;
 }
 
+// C Kodu
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main()
+{
+    setlocale(LC_ALL, "TURKISH");
+    char t_buffer[20];
+    char r_buffer[20] = "_";
+
+    printf("Transmited Buffer= ");
+    scanf("%s", t_buffer);
+    printf("Transmited Length= %zu\n", strlen(t_buffer));
+
+    for (int i = 0; i < strlen(t_buffer); i++)
+    {
+        bool repeat = false;
+        for (int j = 0; j < strlen(t_buffer); j++)
+        {
+            if (i != j && t_buffer[i] == t_buffer[j])
+            {
+                repeat = true;
+                break;
+            }
+        }
+
+        if (repeat) // parameter to pass to the main array with the repeating part condition (to be transmitted over the other array
+        {
+            r_buffer[strlen(r_buffer)] = t_buffer[i];
+        }
+    }
+
+    printf("Returned Buffer= %s\n", r_buffer);
+
+    return 0;
+}
